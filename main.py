98b9e578e200ee4664279ee533fc345de8f0b580
alexanderkjackson <alexanderkjackson@gmail.com>
2014-05-03 06:31:27 -0500

    Add  menu bar, History class, Preferences class, dummy functions to be elaborated on, vim folds, xboard bitmaps, bitmap loading example, vairous TODO dummy functions and TODO comments throughout the code, a stupid, obligatory help window, and much-needed tender, loving care. Refactoring of code to abide somewhat by the style guide. Move create_board to the Application class. A new library is required to run this code: PIL. The license must be changed to GPL because the use of GLP'd bitmaps by xboard.

--- main.py
+++ main.py
@@ -1,58 +1,173 @@
-# REMEMBER - VERY IMPORTANT - Since the move to Python3, many parts of tkinter have been stupidly renamed. Case in point: tKinter is now tkinter. Getting it wrong throws an error and halts the program. I will try to update the wiki with this.
-# Remember to over-comment the source code because we are Python noobs.
-# Remember to use Python3.4
-# Read up on how tkinter actually works
-# Indent properly; it's required in Python
-# I'm sure you'll at least do as well as I am, which is terribly.
-# Oh god how do i orient objects iv been using c too long shit
+#!/usr/bin/python3.4
+#<-- README
+# The first comment line allows UNIX/Linux shells to start the program without the user explicitly invoking Python.
+# For TCL/tk colors, see: http://wiki.tcl.tk/16166
+# These bitmaps are too large; we must resize them!
+#<-- If you're using vim or gvim,
+# you'll be less pissed with:
+#   :syntax on
+#   :set number
+#   :set foldmethod=marker     ]_
+#   :set foldmarker=#<--,#-->  ]  These combined fold the text sections as defined by the style guide. You'll love it!
+#   :set autoindent 
+#   :set formatoptions+=rt
+#   :set textwidth=120 
+#   :help - Displays vim help
+#   :help "option" - Explains any of the options above, like formatoptions
+#   zM - Closes all folds.
+#   zo - Closes the fold at the cursor.
+#   zc - Closes the fold at the cursor.
+#   za - Toggles folding at the cursor.
+# Does vim say "recording" at the bottom-left? You accidentally hit "q" and another key, which makes it record key strokes so you can
+# automate sequences of commands and store it to a buffer unique to that key. Press "q" again to turn it of.
+# You can enter the above options in your vimrc file (usually /etc/vimrc) so you don't have to type them everytime you
+# start vim.
+# A fantastic plugin for vim is supertab. I use supertab because I like long variable and function names.
+#-->
+# I made a style guide at https://github.com/alexanderkjackson/npdm/wiki/Style-guide. I was hoping you'd like it,
+#   but it is very thorough and you don't have to use it at all. If you have suggestions that would make it better,
+#   add them to the page so I can use them.
+# If I make any changes you find disagreeable/stupid/hard-to-use, let me know and revert them if you want.
+# I'll try to keep things organized and easy to use.
+# The imports use '*', which is dangerous in large software projects because of namespace conflicts. That's
+# unlikely to happen here.
+#-->
+#<-- History class - TODO: NYI: Will be a simple linked list storing moves in chess notation
+class History:
+   def undo(self):
+      print("history.undo: Not yet implemented")
+   def redo(self):
+      print("history.redo: Not yet implemented")
+   def __init__(self):
+      print("history.__init__: Not yet implemented")
+   def __del__(self):
+      print("history.__del__: Not yet implemented")
+#-->
+#<-- Preferences class - TODO: NYI: A collection of preferences and a GUI to configure them
+class Preferences:
+   def config_window(self):
+      window = Toplevel()
+      window.title("Preferences")
 
-from tkinter import * # We're not going to have any namespace conflict here. I refuse to obey good and overly-strict methodology for a program that shouldn't be more than a few thousand lines.
-from tkinter import messagebox
+      message = Message(window, text="config_window: Not yet implemented, of course")
+      message.pack()
 
+      button = Button(window, text="Close", command=window.destroy)
+      button.pack()
+
+   def __init__(self):
+      self.difficulty = 0
+   def __del__(self):
+      print("Preferences.__del__: Not yet implemented")
+#-->
+#<-- Application class - TODO: Resize bitmaps, find the center of squares, place pieces to set the board.
 class Application:
-	def __init__(self, master):
-		frame = Frame(master)
-		frame.pack()
+#<-- help_window
+   def help_window(self):
+      messagebox.showinfo(
+        "Help",
+	"I am error."
+	)
+#-->
+#<-- show_authors
+   def show_authors(self):
+      messagebox.showinfo(
+        "Authors",
+        "froman44 and copyright_2014 made this earth-shattering program to fill the void." 
+        ) # TODO: I have an incredibly strong desire to put a function here to construct a dependent clause using Google
+	  # each time it is called.
+#-->
+#<-- Chess board constructor SHOULD: This be moved to its own class and given its own interface?
+   def createBoard(self,master):
+# Creates the canvas on which we render the chessboard.
+      self.canvas = Canvas(master, width=198, height=198)
 
-		self.quitButton = Button(
-		  frame, text="Quit", command=frame.quit
-		  )
-		self.quitButton.pack(side=RIGHT)
+# Draws a "white" rectangle in the canvas.
+      self.canvas.create_rectangle(0, 0, 200, 200, fill="deep sky blue")
+#<-- Fills every other "square" on the board with "black" to make a grid. Colors changed for visibility of pieces.
+      for x in range(0, 200, 25): 
+        self.canvas.create_rectangle(x, x, x + 25, x + 25, fill="sienna")
+        self.canvas.create_rectangle(x, x + 50, x + 25, x + 75, fill="sienna")
+        self.canvas.create_rectangle(x + 50, x, x + 75, x + 25, fill="sienna")
+        self.canvas.create_rectangle(x + 100, x, x + 125, x + 25, fill="sienna")
+        self.canvas.create_rectangle(x + 150, x, x + 175, x + 25, fill="sienna")
+        self.canvas.create_rectangle(x, x + 100, x + 25, x + 125, fill="sienna")
+        self.canvas.create_rectangle(x, x + 150, x + 25, x + 175, fill="sienna")
+#--> 
+#<-- TODO: NYI: Load all bitmaps to the appropriate name. It is also important to have a function that quickly and
+     # easily will find the center of a square. I have no idea how to resize these bitmaps, though.
+      self.white_queen = BitmapImage(file="./bitmaps/q95o.bm",foreground="black")
+      self.image = self.canvas.create_image(100,100,image=self.white_queen)
+#-->
+#-->
+#<-- TODO: NYI
+#<-- force_move
+   def force_move(self):
+      print("force_move: Not yet implemented, and that's cheating.")
+#-->
+#<-- load_game - Will use tkinter file select, allowing *.chess files
+   def load_game(self):
+      print("load_game: Not yet implemented.")
+#-->
+#<-- save_game - Will use tkinter file select
+   def save_game(self):
+      print("save_game: Not yet implemented.")
+#-->
+#-->
+#<-- Menu constructor
+   def make_menu(self,master):
+      self.menu_bar = Menu(master)
+#<-- Cascading "File" menu
+      self.file_menu = Menu(self.menu_bar, tearoff=False)
+      self.file_menu.add_command(label="Load game", command=self.load_game)
+      self.file_menu.add_command(label="Save game", command=self.save_game)
+      self.file_menu.add_separator()
+      self.file_menu.add_command(label="Quit", command=master.quit)
 
-		self.aboutButton = Button(
-		  frame, text="Authors", command=self.aboutWindow
-		  )
-		self.aboutButton.pack(side=LEFT)
+      self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+#-->
+#<-- Cascading "Edit" menu
+      self.edit_menu = Menu(self.menu_bar, tearoff=0)
+      self.edit_menu.add_command(
+        label="Preferences", command=self.preferences.config_window
+	)
+      self.edit_menu.add_command(label="Undo", command=self.history.undo)
+      self.edit_menu.add_command(label="Redo", command=self.history.redo)
+      self.edit_menu.add_command(label="Force move", command=self.force_move)
 
-	def aboutWindow(self):
-		messagebox.showinfo(
-		  "Authors",
-		  "FireproVoltron and copyright_2014 made this earth-shattering program.\nIt was a labor of love."
-		  )
+      self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
+#-->
+#<-- Cascading "About" menu
+      self.about_menu = Menu(self.menu_bar, tearoff=0)
+      self.about_menu.add_command(label="Authors", command=self.show_authors)
+      self.about_menu.add_command(label="Help", command=self.help_window)
 
-#Function to create the 8x8 chess board
-def createBoard():
-    #creates the window
-    w = Canvas(master, width=198, height=198)
-    w.pack()
+      self.menu_bar.add_cascade(label="About", menu=self.about_menu)
+#-->
+#-->
+#<-- Main constructor
+   def __init__(self, master):
 
-    #Makes the whole window white to later allow for alternating black and white
-    #squares
-    w.create_rectangle(0, 0, 200, 200, fill="white")
+     self.createBoard(master)
+     self.canvas.pack()
 
-    #Fills every other "square" on the board with black to make a grid.
-    for x in range(0, 200, 25):
-        w.create_rectangle(x, x, x + 25, x + 25, fill="black")
-        w.create_rectangle(x, x + 50, x + 25, x + 75, fill="black")
-        w.create_rectangle(x + 50, x, x + 75, x + 25, fill="black")
-        w.create_rectangle(x + 100, x, x + 125, x + 25, fill="black")
-        w.create_rectangle(x + 150, x, x + 175, x + 25, fill="black")
-        w.create_rectangle(x, x + 100, x + 25, x + 125, fill="black")
-        w.create_rectangle(x, x + 150, x + 25, x + 175, fill="black")
+     self.history = History()
+     self.preferences = Preferences()
 
+     self.make_menu(master)
+     master.config(menu=self.menu_bar)
+#-->
+#-->
+#<-- Imports
+from tkinter import * 
+from tkinter import messagebox
+from PIL import *
+#-->
+#<-- Bootstrapping
 root = Tk()
 
 app = Application(root)
 
 root.mainloop()
 root.destroy()
+#--> 
