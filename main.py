5f523d8a24c3301f7b203e828327c7ffb990c00f
alexanderkjackson <alexanderkjackson@gmail.com>
2014-05-03 09:52:40 -0500

    Negate most issues of the last commit. Pieces are now filled in. More important board functions. Need to base rendering off of bit_board instead of just the demonstration board shown. Implement 1/3/3/5 grading system and piece encoding/decoding between the grading system and the convenient reference numbers used to initialize the board.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #<-- README
 # The first comment line allows UNIX/Linux shells to start the program without the user explicitly invoking Python.
 # For TCL/tk colors, see: http://wiki.tcl.tk/16166
-# These bitmaps are too large; we must resize them!
+# We may want to use Tix, IF we can figure out how to make it actually draw a pixmap to a canvas.
 #<-- If you're using vim or gvim,
 # you'll be less pissed with:
 #   :syntax on
@@ -31,6 +31,160 @@
 # I'll try to keep things organized and easy to use.
 # The imports use '*', which is dangerous in large software projects because of namespace conflicts. That's
 # unlikely to happen here.
+#-->
+#<-- Piece definitions - This is a rare example of predefined values that may be global. Is there a better way?
+# Do not change these definitions. They have been carefully chosen for quick/efficient board weighing.
+white_pawn = 21
+white_rook = 109
+white_knight = 61
+white_bishop = 63
+white_queen = 191
+white_king = 255
+black_pawn = 22
+black_rook = 110
+black_knight = 62
+black_bishop = 64
+black_queen = 192
+black_king = 256
+#-->
+#<-- Board class
+class Board:
+   def x(self,n): # Tpls are 4 noobs.
+      return (n*25+12.5)
+   def y(self,n):
+      return (n*25+12.5)
+#<-- Encoding - Relates image number to piece number
+   def encode_black_piece(self,piece_number):
+     return {
+       0 : 110,
+       1 : 62,
+       2 : 64,
+       3 : 192,
+       4 : 256,
+       5 : 64,
+       6 : 62,
+       7 : 110,
+       8 : 22, # Pawn
+     }
+   def encode_white_piece(self,piece_number):
+     return {
+       0 : 109,
+       1 : 61,
+       2 : 63,
+       3 : 191,
+       4 : 255,
+       5 : 63,
+       6 : 61,
+       7 : 110,
+       8 : 21, # Pawn
+     }
+#-->
+#<-- Decoding - Relates piece number to image number
+   def decode_black_piece(self,piece_number):
+     return {
+       110 : 0,
+       62 : 1,
+       64 : 2,
+       192 : 3,
+       256 : 4,
+       64 : 5,
+       62 : 6,
+       110 : 7,
+       22 : 8, # Pawn
+     }
+   def decode_white_piece(self,piece_number):
+     return {
+       109 : 0,
+       61 : 1,
+       63 : 2,
+       191 : 3,
+       255 : 4,
+       63 : 5,
+       61 : 6,
+       109 : 7,
+       21 : 8, # Pawn
+     }
+#-->
+#<-- Board functions: board_init, board_reset, and board_render TODO: Incomplete.
+#   def board_init: # TODO: I didn't quite have time to make a bitboard, set it up, and use it. 
+#      for inc in range(7):
+#        self.bit_board[inc][6] = white_pawn
+#	self.
+#   def board_reset:
+#     for inc_x in range(0,7):
+#       self.bit_board[inc_x][6] = white_pawn
+#       self.bit_board[inc_x][1] = black_pawn
+#     for inc_y in range(0,7):
+#       self.bit_board[0] = encode_black_piece(inc_y)
+#       self.bit_board[7] = encode_white_piece(inc_y)
+#   def board_render:
+#      for inc_x in range(0,7):
+#        for inc_y in range(0,7):
+#	  self.canvas.create_image(
+#	    self.x(inc_x), self.y(inc_y),
+#	    image=self.decode_piece(self.bit_board[inc_x][inc_y])
+#      
+#
+#-->
+#<-- constructor
+   def __init__(self,master):
+# Creates the canvas on which we render the chessboard.
+      self.bit_board = [[0 for inc in range(7)] for inc in range(7)]
+      self.canvas = Canvas(master, width=198, height=198)
+
+# Draws a "black" rectangle in the canvas.
+      self.canvas.create_rectangle(0, 0, 200, 200, fill="sienna")
+#<-- Fills every other "square" on the board with "white" to make a grid. Colors changed for visibility of pieces aand
+# so the white side faces the player 
+      for x in range(0, 200, 25): 
+        self.canvas.create_rectangle(x, x, x + 25, x + 25, fill="deep sky blue")
+        self.canvas.create_rectangle(x, x + 50, x + 25, x + 75, fill="deep sky blue")
+        self.canvas.create_rectangle(x + 50, x, x + 75, x + 25, fill="deep sky blue")
+        self.canvas.create_rectangle(x + 100, x, x + 125, x + 25, fill="deep sky blue")
+        self.canvas.create_rectangle(x + 150, x, x + 175, x + 25, fill="deep sky blue")
+        self.canvas.create_rectangle(x, x + 100, x + 25, x + 125, fill="deep sky blue")
+        self.canvas.create_rectangle(x, x + 150, x + 25, x + 175, fill="deep sky blue")
+#--> 
+#<-- Bitmap loading and rendering. TODO: We need to resize the board, because 25 % 2 = 1, so pieces are off by half a
+# pixel. It's noticeable.
+      self.white_rook = BitmapImage(file="./bitmaps/r25s.bm",foreground="white")
+      self.white_knight = BitmapImage(file="./bitmaps/n25s.bm",foreground="white")
+      self.white_bishop = BitmapImage(file="./bitmaps/b25s.bm",foreground="white")
+      self.white_queen = BitmapImage(file="./bitmaps/q25s.bm",foreground="white")
+      self.white_king = BitmapImage(file="./bitmaps/k25s.bm",foreground="white")
+      self.white_pawn = BitmapImage(file="./bitmaps/p25s.bm",foreground="white") # My fingers are tired. :(
+      self.black_rook = BitmapImage(file="./bitmaps/r25s.bm",foreground="black")
+      self.black_knight = BitmapImage(file="./bitmaps/n25s.bm",foreground="black")
+      self.black_bishop = BitmapImage(file="./bitmaps/b25s.bm",foreground="black")
+      self.black_queen = BitmapImage(file="./bitmaps/q25s.bm",foreground="black")
+      self.black_king = BitmapImage(file="./bitmaps/k25s.bm",foreground="black")
+      self.black_pawn = BitmapImage(file="./bitmaps/p25s.bm",foreground="black")
+
+      # If these don't show up later after changing the canvas, we need to keep a reference here because Python may be
+      # collecting them as garbage.
+      self.canvas.create_image(self.x(7),self.y(7),image=self.white_rook)
+      self.canvas.create_image(self.x(0),self.y(7),image=self.white_rook)
+      self.canvas.create_image(self.x(1),self.y(7),image=self.white_knight)
+      self.canvas.create_image(self.x(6),self.y(7),image=self.white_knight)
+      self.canvas.create_image(self.x(2),self.y(7),image=self.white_bishop)
+      self.canvas.create_image(self.x(5),self.y(7),image=self.white_bishop)
+      self.canvas.create_image(self.x(3),self.y(7),image=self.white_queen)
+      self.canvas.create_image(self.x(4),self.y(7),image=self.white_king)
+      for inc in range(0, 8):
+         self.canvas.create_image(self.x(inc),self.y(6),image=self.white_pawn)
+
+      self.canvas.create_image(self.x(7),self.y(0),image=self.black_rook)
+      self.canvas.create_image(self.x(0),self.y(0),image=self.black_rook)
+      self.canvas.create_image(self.x(1),self.y(0),image=self.black_knight)
+      self.canvas.create_image(self.x(6),self.y(0),image=self.black_knight)
+      self.canvas.create_image(self.x(2),self.y(0),image=self.black_bishop)
+      self.canvas.create_image(self.x(5),self.y(0),image=self.black_bishop)
+      self.canvas.create_image(self.x(3),self.y(0),image=self.black_queen)
+      self.canvas.create_image(self.x(4),self.y(0),image=self.black_king)
+      for inc in range(0, 8):
+         self.canvas.create_image(self.x(inc),self.y(1),image=self.black_pawn)
+#-->
+#-->
 #-->
 #<-- History class - TODO: NYI: Will be a simple linked list storing moves in chess notation
 class History:
@@ -76,29 +230,6 @@ class Application:
         "froman44 and copyright_2014 made this earth-shattering program to fill the void." 
         ) # TODO: I have an incredibly strong desire to put a function here to construct a dependent clause using Google
 	  # each time it is called.
-#-->
-#<-- Chess board constructor SHOULD: This be moved to its own class and given its own interface?
-   def createBoard(self,master):
-# Creates the canvas on which we render the chessboard.
-      self.canvas = Canvas(master, width=198, height=198)
-
-# Draws a "white" rectangle in the canvas.
-      self.canvas.create_rectangle(0, 0, 200, 200, fill="deep sky blue")
-#<-- Fills every other "square" on the board with "black" to make a grid. Colors changed for visibility of pieces.
-      for x in range(0, 200, 25): 
-        self.canvas.create_rectangle(x, x, x + 25, x + 25, fill="sienna")
-        self.canvas.create_rectangle(x, x + 50, x + 25, x + 75, fill="sienna")
-        self.canvas.create_rectangle(x + 50, x, x + 75, x + 25, fill="sienna")
-        self.canvas.create_rectangle(x + 100, x, x + 125, x + 25, fill="sienna")
-        self.canvas.create_rectangle(x + 150, x, x + 175, x + 25, fill="sienna")
-        self.canvas.create_rectangle(x, x + 100, x + 25, x + 125, fill="sienna")
-        self.canvas.create_rectangle(x, x + 150, x + 25, x + 175, fill="sienna")
-#--> 
-#<-- TODO: NYI: Load all bitmaps to the appropriate name. It is also important to have a function that quickly and
-     # easily will find the center of a square. I have no idea how to resize these bitmaps, though.
-      self.white_queen = BitmapImage(file="./bitmaps/q95o.bm",foreground="black")
-      self.image = self.canvas.create_image(100,100,image=self.white_queen)
-#-->
 #-->
 #<-- TODO: NYI
 #<-- force_move
@@ -147,21 +278,20 @@ class Application:
 #-->
 #<-- Main constructor
    def __init__(self, master):
+     
+      self.board = Board(master)
+      self.board.canvas.pack() # I'm maybe going overboard with OO, but they were convincing!
 
-     self.createBoard(master)
-     self.canvas.pack()
+      self.history = History()
+      self.preferences = Preferences()
 
-     self.history = History()
-     self.preferences = Preferences()
-
-     self.make_menu(master)
-     master.config(menu=self.menu_bar)
+      self.make_menu(master)
+      master.config(menu=self.menu_bar)
 #-->
 #-->
 #<-- Imports
 from tkinter import * 
 from tkinter import messagebox
-from PIL import *
 #-->
 #<-- Bootstrapping
 root = Tk()
